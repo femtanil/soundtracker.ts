5f32555adf3ebb64e9c39f99db8def03a861ccd7
femtanil <femtanil@gmail.com>
2024-07-23 17:57:25 +0200

    Added master volume component

---
+++ src/components/AppMasterVolume.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app-master-volume-container" class="flex flew-row">
+        <AppRangeBar class="m-1"></AppRangeBar>
+        <AppSmallButton class="m-1">{{ 'V' }}</AppSmallButton>
+    </div>
+</template>
+<script setup lang="ts">
+import AppRangeBar from '@/components/AppRangeBar.vue';
+import AppSmallButton from '@/components/AppSmallButton.vue';
+</script>
