e427cdb5fcffa5e48b12257aa412b094c467af3a
femtanil <femtanil@gmail.com>
2024-07-23 17:57:02 +0200

    Added generic range bar component

---
+++ src/components/AppRangeBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="app-range-bar-container">
+        <AppInput v-model="props.valueInput" type="range" :class="'range range-secondary'" :step="props.step"></AppInput>
+    </div>
+</template>
+<script setup lang="ts">
+import AppInput from '@/components/AppInput.vue';
+
+interface Props {
+    valueInput?: number;
+    maxValue?: number;
+    step?: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    valueInput: () => 33,
+    maxValue: () => 100,
+    step: () => 12.5,
+});
+</script>
