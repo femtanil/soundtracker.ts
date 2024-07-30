94be0bcc6e10ad35bb8996952fba04711d6a6eeb
femtanil <femtanil@gmail.com>
2024-07-30 17:31:43 +0200

    Added inputs to change the number of tracks, steps and BPM

--- src/components/Sequencer.vue
+++ src/components/Sequencer.vue
@@ -1,9 +1,31 @@
 <template>
     <div id="sequencer-container">
-        <div v-for="track in tracks" :key="track.id">
+        <div id="sequencer-container-settings" class="flex flex-row justify-center">
+            <div class="flex flew-row">
+                <div class="">
+                    <span>
+                        Tracks:
+                    </span>
+                    <AppInput v-model="inputTracks" class="input input-bordered input-xs text-center w-10"></AppInput>
+                </div>
+                <div class="">
+                    <span>
+                        Steps:
+                    </span>
+                    <AppInput v-model="inputSteps" class="input input-bordered input-xs text-center w-10"></AppInput>
+                </div>
+                <div class="">
+                    <span>
+                        BPM:
+                    </span>
+                    <AppInput v-model="inputBpm" class="input input-bordered input-xs text-center w-10"></AppInput>
+                </div>
+            </div>
+        </div>
+        <div id="sequencer-container-tracks" v-for="track in tracks" :key="track.id">
             <Track :track-id="track.id" :steps="track.steps" />
         </div>
-        <div class="flex justify-center">
+        <div id="sequencer-container-playback" class="flex justify-center">
             <AppSmallButton @click="sequencerService?.playSequenceOneTrackPerStep()">{{ '▶️' }}</AppSmallButton>
             <AppSmallButton @click="sequencerService?.stopSequence()">{{ '⏹️' }}</AppSmallButton>
         </div>
@@ -11,17 +33,24 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, inject } from 'vue';
+import { watch, inject, ref, Ref } from 'vue';
 import { storeToRefs } from 'pinia';
+import AppInput from '@/components/AppInput.vue';
 import AppSmallButton from '@/components/AppSmallButton.vue';
 import Track from '@/components/Track.vue';
 import { useSequencerStore } from '@/stores/sequencerStore.ts';
 import { sequencerServiceKey } from '@/utils/injection-keys';
 import { SequencerService } from '@/services/SequencerServices';
 
-const { tracks } = storeToRefs(useSequencerStore());
+const { tracks, numTracks, numSteps, bpm } = storeToRefs(useSequencerStore());
 const sequencerService = inject<SequencerService>(sequencerServiceKey);
+const inputTracks: Ref<number> = ref(numTracks.value);
+const inputSteps: Ref<number> = ref(numSteps.value);
+const inputBpm: Ref<number> = ref(bpm.value);
 
-onMounted(() => {
-});
+watch([inputTracks, inputSteps], ([newInputTracks, newInputSteps]) => {
+    sequencerService?.stopSequence();
+    numTracks.value = newInputTracks;
+    inputSteps.value = newInputSteps;
+})
 </script>
